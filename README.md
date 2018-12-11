10f30f47612c902a125ba9d41fa07bfe228d0409
Abdou Bouroubi <abdou@mbp-de-abdou.home>
2018-12-11 22:13:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,134 @@
+# passport-google-verify-token
+
+![License](https://img.shields.io/npm/l/passport-facebook-token.svg)
+
+
+[Passport](http://passportjs.org/) strategy for authenticating with [Google](http://www.google.com/)
+access tokens using the OAuth 2.0 API.
+
+This module lets you authenticate using Google in your Node.js applications.
+By plugging into Passport, Google authentication can be easily and
+unobtrusively integrated into any application or framework that supports
+[Connect](http://www.senchalabs.org/connect/)-style middleware, including
+[Express](http://expressjs.com/).
+
+## Installation
+
+    $ npm install passport-google-verify-token
+
+## Usage
+
+### Configure Strategy
+
+The Google authentication strategy leverages the [Google Auth Library for Node.js](https://github.com/googleapis/google-auth-library-nodejs) to authenticates users. 
+Applications must supply a `verify` callback which accepts the `idToken`
+coming from the user to be authenticated, and then calls the `done` callback
+supplying a `parsedToken` (with all its information in visible form) and the
+`googleId`.
+
+```js
+passport.use(new GoogleTokenStrategy({
+      clientID: '12345.abcdefghijkl.apps.googleusercontent.com'// Specify the CLIENT_ID of the app that accesses the backend
+     // Or, if multiple clients access the backend:
+     //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    },
+    function(parsedToken, googleId, done) {
+      User.findOrCreate(..., function (err, user) {
+        done(err, user);
+      });
+    }
+  ));
+```
+
+### Authenticate Requests
+
+Use `passport.authenticate()`, specifying the `'google-verifiy-token'` strategy, to authenticate requests.
+
+```js
+app.post('/auth/google/token',
+  passport.authenticate('google-verifiy-token'),
+  function (req, res) {
+    // do something with req.user
+    res.send(req.user? 200 : 401);
+  }
+);
+```
+
+Or using Sails framework:
+
+```javascript
+// api/controllers/AuthController.js
+module.exports = {
+  facebook: function(req, res) {
+    passport.authenticate('google-verifiy-token', function(error, user, info) {
+      // do stuff with user
+      res.ok();
+    })(req, res);
+  }
+};
+```
+
+### Client Requests
+
+Clients can send requests to routes that use google-verifiy-token authentication using query parms, body, or HTTP headers. Clients will need to transmit the `access_token` or the `id_token` that are received from Google after user logs in.
+
+#### Sending access_token as a Query parameter
+
+```
+GET /auth/google/token?access_token=<TOKEN_HERE>
+```
+
+#### Sending access token as an HTTP header
+
+Clients can choose to send the access token using the Oauth2 Bearer token (RFC 6750) compliant format
+
+```
+GET /resource HTTP/1.1
+Host: server.example.com
+Authorization: Bearer base64_access_token_string
+```
+
+
+#### Sending access token as an HTTP body
+
+Clients can transmit the access token via the body
+
+```
+POST /resource HTTP/1.1
+Host: server.example.com
+
+access_token=base64_access_token_string
+```
+  
+
+## Credits
+
+  - [Abdou BOUROUBI](http://github.com/abouroubi)
+
+## Thanks
+
+This project is inspired by [Juanma Reyes](http://github.com/jmreyes)' [Passport Google Id Token](https://github.com/jmreyes/passport-google-id-token).
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2018 Abdou Bouroubi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ src/strategy.ts
@@ -0,0 +1,171 @@
+/**
+ * Module dependencies.
+ */
+import { OAuth2Client } from 'google-auth-library';
+import { Strategy } from 'passport-strategy';
+
+/**
+ * `Strategy` constructor.
+ *
+ * The Google authentication strategy authenticates requests by verifying the
+ * signature and fields of the token.
+ *
+ * Applications must supply a `verify` callback which accepts the `idToken`
+ * coming from the user to be authenticated, and then calls the `done` callback
+ * supplying a `parsedToken` (with all its information in visible form) and the
+ * `googleId`.
+ *
+ * Options:
+ * - `clientID` your Google application's client id (or several as Array)
+ *
+ * Examples:
+ *
+ * passport.use(new GoogleTokenStrategy({
+ *     clientID: '12345.abcdefghijkl.apps.googleusercontent.com'// Specify the CLIENT_ID of the app that accesses the backend
+ *    // Or, if multiple clients access the backend:
+ *    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+ *   },
+ *   function(parsedToken, googleId, done) {
+ *     User.findOrCreate(..., function (err, user) {
+ *       done(err, user);
+ *     });
+ *   }
+ * ));
+ *
+ * @param {Object} options
+ * @param {Function} verify
+ * @api public
+ */
+export class GoogleTokenStrategy extends Strategy {
+  public name: string;
+  public googleAuthClient: OAuth2Client;
+  public verify: (...args: any[]) => void;
+  public clientID: string;
+  public passReqToCallback: boolean;
+
+  constructor(options: (() => void) | any, verify?: (...args: any[]) => void) {
+    super();
+
+    if (typeof options === 'function') {
+      verify = options;
+      options = {};
+    }
+
+    if (!verify) {
+      throw new Error('GoogleVerifyTokenStrategy requires a verify function');
+    }
+
+    this.passReqToCallback = options.passReqToCallback;
+
+    this.clientID = options.clientID;
+
+    this.name = 'google-verify-token';
+    this.googleAuthClient = new OAuth2Client(this.clientID);
+    this.verify = verify;
+  }
+
+  /**
+   * Authenticate request by verifying the token
+   *
+   * @param {Object} req
+   * @api protected
+   */
+  public authenticate(req: any, options: any) {
+    options = options || {};
+
+    const idToken =
+      this.paramFromRequest(req, 'id_token') ||
+      this.paramFromRequest(req, 'access_token') ||
+      this.getBearerToken(req.headers);
+
+    if (!idToken) {
+      return this.fail({ message: 'no ID token provided' }, 401);
+    }
+
+    this.verifyGoogleToken(idToken, this.clientID, (err: any, parsedToken: any, info: any) => {
+      if (err) {
+        return this.fail({ message: err.message }, 401);
+      }
+
+      if (!parsedToken) {
+        return this.fail(info);
+      }
+
+      const verified = (error: any, user: any, infoOnUser: any) => {
+        if (error) {
+          return this.error(error);
+        }
+        if (!user) {
+          return this.fail(infoOnUser);
+        }
+        this.success(user, infoOnUser);
+      };
+
+      if (this.passReqToCallback) {
+        this.verify(req, parsedToken, parsedToken.sub, verified);
+      } else {
+        this.verify(parsedToken, parsedToken.sub, verified);
+      }
+    });
+  }
+
+  /**
+   * Verify signature and token fields
+   *
+   * @param {String} idToken
+   * @param {String} clientID
+   * @param {Function} done
+   * @api protected
+   */
+  public verifyGoogleToken(idToken: string, clientID: string | [], done: (...args: any[]) => void) {
+    this.googleAuthClient.verifyIdToken(
+      {
+        audience: clientID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        // [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        idToken,
+      },
+      (err, loginTicket) => {
+        if (err) {
+          done(null, false, { message: err.message });
+        } else if (loginTicket) {
+          const payload = loginTicket.getPayload();
+          done(null, payload);
+        } else {
+          done(null, false, { message: 'No login ticket retuned' });
+        }
+      },
+    );
+  }
+
+  /**
+   * Gets the id token value from req using name for lookup in req.body, req.query,
+   * and req.params.
+   * @param {express.Request} req
+   * @param {string} name  the key to use to lookup id token in req.
+   * @api protected
+   */
+  private paramFromRequest(req: any, name: string) {
+    const body = req.body || {};
+    const query = req.query || {};
+    const params = req.params || {};
+    const headers = req.headers || {};
+    if (body[name]) {
+      return body[name];
+    }
+    if (query[name]) {
+      return query[name];
+    }
+    if (headers[name]) {
+      return headers[name];
+    }
+    return params[name] || '';
+  }
+
+  private getBearerToken(headers: any) {
+    if (headers && headers.authorization) {
+      const parts = headers.authorization.split(' ');
+      return parts.length === 2 && parts[0] === 'Bearer' ? parts[1] : undefined;
+    }
+  }
+}
